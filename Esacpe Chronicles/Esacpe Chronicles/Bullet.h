c1ca731429231d5fca50e2a75bba91175403709a
unknown <kevin018111@gmail.com>
2024-06-13 18:00:31 +0900

    update: bullet.h 수정

--- Esacpe Chronicles/Esacpe Chronicles/Bullet.h
+++ Esacpe Chronicles/Esacpe Chronicles/Bullet.h
@@ -4,40 +4,16 @@
 
 class Bullet {
 public:
+
 	LPCTSTR _arrow_r[1] = {
 		L"img/character/main/weapon/bow/arrow.png"
 	};
 
-	LPCTSTR _arrow_l[2] = {
-		L"img/character/main/weapon/bow/arrow_l.png"
+	LPCTSTR _bullet_r[1] = {
+		L"img/character/main/weapon/gun/8.png"
 	};
 
-	std::unique_ptr<CImage> cImage;
+	Gdiplus::Image* img = nullptr;
 	RECT rect;
-
-	Bullet() :cImage(std::make_unique<CImage>()) {
-		HRESULT hr3 = cImage->Load(_arrow_r[0]);
-
-		if (FAILED(hr3)) {
-			MessageBox(NULL, L"총알 이미지 로드 실패", L"오류", MB_OK);
-		}
-	}
-
-	//// Move constructor
-	//Bullet(Bullet&& other) noexcept
-	//	: cImage(std::move(other.cImage)), rect(other.rect) {}
-
-	//// Move assignment operator
-	//Bullet& operator=(Bullet&& other) noexcept {
-	//	if (this != &other) {
-	//		cImage = std::move(other.cImage);
-	//		rect = other.rect;
-	//	}
-	//	return *this;
-	//}
-
-	//// Disable copy constructor and copy assignment operator
-	//Bullet(const Bullet&) = delete;
-	//Bullet& operator=(const Bullet&) = delete;
 
 };
