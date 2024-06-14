ef5c6220f71f56eede954d243add86bdc9bc23f2
zhaominn <150769172+zhaominn@users.noreply.github.com>
2024-06-14 18:47:34 +0900

    fix:총 방향 바꿈

--- Esacpe Chronicles/Esacpe Chronicles/StageManager.cpp
+++ Esacpe Chronicles/Esacpe Chronicles/StageManager.cpp
@@ -69,19 +69,19 @@ void StageManager::setBlock(const int& h, const LPCTSTR& path_block, const float
 	}
 	*/
 	for (int i = 0; i < (game_rect.right / BLOCK_SIZE) - 1; ++i) {
-		if(i%30 <3){
-			Block block;
-			block.color = RGB(62, 141, 215);
-			block.rect = { ((i%30) + 2) * BLOCK_SIZE, 0, ((i%30) + 3) * BLOCK_SIZE, BLOCK_SIZE };
-
-			OffsetRect(&block.rect, 0, this->game_rect.bottom / 10 * (9 - (i%30)));
-			blocks_stage1.push_back(std::move(block));
-		}
-		else if (i%4!=1&&i % 7 != 1 && i % 11 != 1) {
+		if(i%35 <3){
 			Block block;
 			block.color = RGB(62, 141, 215);
 			block.rect = { (i + 2) * BLOCK_SIZE, 0, (i + 3) * BLOCK_SIZE, BLOCK_SIZE };
-			if(i%30<5){
+
+			OffsetRect(&block.rect, 0, this->game_rect.bottom / 10 * (9 - (i%35)));
+			blocks_stage1.push_back(std::move(block));
+		}
+		else if (i%4!=1||i % 7 != 1 || i % 11 != 1) {
+			Block block;
+			block.color = RGB(62, 141, 215);
+			block.rect = { (i + 2) * BLOCK_SIZE, 0, (i + 3) * BLOCK_SIZE, BLOCK_SIZE };
+			if(i%7==2||i%7==0||i%11==2||i%11==0){
 				OffsetRect(&block.rect, 0, this->game_rect.bottom / 10 * 6);
 			}
 			else {
