b53e8752c371de6429698004535d18bf8347d6df
zhaominn <150769172+zhaominn@users.noreply.github.com>
2024-06-15 23:41:59 +0900

    fix : 뇌몬스터 공격 받으면  뒤로가게 수정

--- Esacpe Chronicles/Esacpe Chronicles/Brain2.cpp
+++ Esacpe Chronicles/Esacpe Chronicles/Brain2.cpp
@@ -77,7 +77,7 @@ void Brain2::move(const StageManager& stageManager) {
 
 	switch (status) {
 	case MOVE_: {
-		int offset = left ? -4 : 4;
+		int offset = left ? -2 : 2;
 		OffsetRect(&rect, offset, 0);
 
 		if (imageNum == 18)
@@ -129,9 +129,9 @@ void Brain2::MonsterPlayerCollision(Player& p) {
 		if (IntersectRect(&intersectRect, &it->rect, &rect)) {
 			attacked = true;
 			if (p.direction == PlayerStatus::RIGHT)
-				OffsetRect(&rect, +20, -20);
+				OffsetRect(&rect, +30, -30);
 			else
-				OffsetRect(&rect, -20, -20);
+				OffsetRect(&rect, -30, -30);
 			it = p.bullets.erase(it); // 해당 총알, 화살 제거
 
 			hp -= p.power;
@@ -153,9 +153,9 @@ void Brain2::Collisionplayer(const Player& p) { //플레이어랑 충돌했을때 몬스터의
 	case ATTACK:
 		attacked = true;
 		if (p.direction == PlayerStatus::RIGHT)
-			OffsetRect(&rect, +20, -20);
+			OffsetRect(&rect, +30, -30);
 		else
-			OffsetRect(&rect, -20, -20);
+			OffsetRect(&rect, -30, -30);
 
 		hp -= p.power;
 		attacksize = p.power;
